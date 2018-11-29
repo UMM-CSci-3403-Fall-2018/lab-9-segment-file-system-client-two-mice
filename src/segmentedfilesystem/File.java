b3f8cf9c0e7c08e0db303979effdbab3b0aee0ea
chong050 <chong050@morris.umn.edu>
2018-11-28 20:30:12 -0600

    Add addHeader, addFooter, addData, isComplete(?), getID methods and File constructor.

--- src/segmentedfilesystem/File.java
+++ src/segmentedfilesystem/File.java
@@ -5,9 +5,45 @@ import java.util.ArrayList;
 public class File {
     String name;
     byte[] header;
+    byte[] footer;
     ArrayList<byte[]> datapacket = new ArrayList<byte[]>();
+    int total = 0;
+    byte id;
 
-    public void dataAdd(byte[] packet) {
-        this.datapacket.add(packet);
+    public void addHeader(byte[] header) {
+        this.header = header;
+    }
+
+    public void addFooter(byte[] footer) {
+        if(!datapacket.contains(footer)){
+            this.datapacket.add(footer);
+            total = footer[2];
+            this.footer = footer;
+        }
+    }
+    public void addData(byte[] packet) {
+        if(!datapacket.contains(packet)){
+            this.datapacket.add(packet);
+        }
+    }
+
+    public File(StringBuilder name, byte id) {
+        this.name = name.toString();
+        this.id = id;
+    }
+
+    public boolean isComplete() {
+        if (total == 0) {
+            return false;
+        }
+        if (datapacket.size() == total) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int getID() {
+        return this.id;
     }
 }
