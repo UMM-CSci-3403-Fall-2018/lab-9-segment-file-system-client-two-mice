e042849c3cd4acf7639c6e1f5b74e9441bc3244b
chong050 <chong050@morris.umn.edu>
2018-11-28 20:32:00 -0600

    Moved packet receive into new while loop. Add end check, head and data check, name getter(?) and file builder.

--- src/segmentedfilesystem/Main.java
+++ src/segmentedfilesystem/Main.java
@@ -42,39 +42,89 @@ public class Main {
             e.printStackTrace();
         }
 
-        // Receive from server
-        packet = new DatagramPacket(buf, buf.length);
-        try {
-            socket.receive(packet);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
 
         /*
         String received = new String(packet.getData(), 0, packet.getLength());
         System.out.println("Quote of the Moment: " + received);
         */
+        StringBuilder str = new StringBuilder("null");
 
-        byte[] received = packet.getData();
-        ArrayList<byte[]> headers = new ArrayList<byte[]>();
+        File file1 = new File(str, Byte.parseByte("0"));
+        File file2 = new File(str, Byte.parseByte("0"));
+        File file3 = new File(str, Byte.parseByte("0"));
 
-        // Header check
-        if (received[0]%2 == 0) {
-            if (!headers.contains(received)) {
-             headers.add(received);
+        ArrayList<byte[]> headers = new ArrayList<>();
+
+        // while loop goes here
+        while(!file1.isComplete() && !file2.isComplete() && !file3.isComplete()) {
+
+            // Receive from server
+            packet = new DatagramPacket(buf, buf.length);
+            try {
+                socket.receive(packet);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            byte[] received = packet.getData();
+
+            // System.out.println(received[0]);
+
+            // End check
+            if (received[0] % 4 == 3) {
+                if (received[1] == file1.getID()) {
+                    file1.addFooter(received);
+                } else if (received[1] == file2.getID()) {
+                    file2.addFooter(received);
+                } else {
+                    file3.addFooter(received);
+                }
+            }
+
+            // Header check and else data check
+            else if (received[0] % 2 == 0) {
+                if (!headers.contains(received)) {
+                    headers.add(received);
+
+                    StringBuilder name = new StringBuilder();
+
+                    // get name of file
+                    for (int i = 2; i < received.length; i++) {
+                        name.append(Byte.toString(received[i]));
+                    }
+
+                    byte id = received[1];
+
+                    if (file1.name.equals("null")) {
+                        file1 = new File(name, id);
+                        file1.addHeader(received);
+                        System.out.println(name + "Print found 1");
+                    } else if (file2.name.equals("null")) {
+                        file2 = new File(name, id);
+                        file2.addHeader(received);
+                        System.out.println("Print found 2");
+                    } else {
+                        file3 = new File(name, id);
+                        file3.addHeader(received);
+                        System.out.println("Print found 3");
+                    }
+                }
+            } else {
+                if (received[1] == file1.getID()) {
+                    file1.addData(received);
+                } else if (received[1] == file2.getID()) {
+                    file2.addData(received);
+                } else {
+                    file3.addData(received);
+                }
             }
         }
 
-        for (int i = 0; i < 4; i++) {
-            String string = Byte.toString(received[i]);
-            System.out.println(string);
-        }
-        // System.out.println(received.length);
-        /*
-        for (int i = 0; i < received.length; i++) {
-            System.out.println(received[i]);
-        }
-        */
+        System.out.println(file1.name);
+        System.out.println(file2.name);
+        System.out.println(file3.name);
+
     }
 
 }
